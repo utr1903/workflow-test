b3138478802520461cc5a8d74bb5327ee6ad4625
uturkarslan <ugurturkarslan@gmail.com>
2024-06-04 13:56:21 +0200

    Test

--- test.py
+++ test.py
@@ -1,26 +1,23 @@
 import os
 import json
 
-try:
-    secret = os.getenv("TEST_SECRET")
-    print(type(secret))
-    print(secret)
 
-    infos = accounts = json.loads("'{}'".format(secret))
-    print(type(infos))
-    print(infos)
+secret = os.getenv("TEST_SECRET")
+print(type(secret))
+print(secret)
 
-    infos = accounts = json.loads(
-        '[{"organizationName":"MY ORG","accountId":"123","accountApiKey":"XXX","accountRegion":"AA"}]'
-    )
-    print(type(infos))
-    print(infos)
+infos = json.loads("'{}'".format(secret))
+print(type(infos))
+print(infos)
 
-    for key, value in infos.items():
-        print(type(key))
-        print(key)
-        print(type(value))
-        print(value)
+infos = json.loads(
+    '[{"organizationName":"MY ORG","accountId":"123","accountApiKey":"XXX","accountRegion":"AA"}]'
+)
+print(type(infos))
+print(infos)
 
-except Exception as e:
-    print(e)
+for key, value in infos.items():
+    print(type(key))
+    print(key)
+    print(type(value))
+    print(value)
